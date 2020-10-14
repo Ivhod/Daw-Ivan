97d2f08fac340940681961e66797562810b5ae21
Ivhod <56817761+Ivhod@users.noreply.github.com>
2020-10-14 09:55:23 +0200

    Add files via upload

---
+++ src/Equacion.java
@@ -0,0 +1,10 @@
+
+public class Equacion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Math.sqrt() Es para hacer la raiz cuadrada
+		//Math.pow(b,2)
+	}
+
+}
