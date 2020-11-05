563e197fae2b69dd778c1a093113744047f933e5
Ivhod <56817761+Ivhod@users.noreply.github.com>
2020-11-05 13:36:49 +0100

    Add files via upload

---
+++ src/RandomCaracter.java
@@ -0,0 +1,11 @@
+
+public class RandomCaracter {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//1-Generar numero caracter del 0 al 9
+		//2-Generar lowcase caracter de A a la Z minuscula
+		//3-Generar upercase caracter de A a la Z mayuscula
+	}
+
+}
