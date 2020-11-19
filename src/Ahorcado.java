a2ddcc865650102b1071c247315abb9ac5878a4a
Ivhod <56817761+Ivhod@users.noreply.github.com>
2020-11-19 19:56:14 +0100

    Add files via upload

---
+++ src/Ahorcado.java
@@ -0,0 +1,30 @@
+import java.util.Random;
+
+public class Ahorcado {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(" Vamos a jugar al Ahorcado ");
+		System.out.println(" Se generara una Palabra aleatoria y tendras que aceptarlas");
+
+		String palabra;
+		String[] diccionario = new String[] { "perro", "mando", "juntar", "portatil", "movil", "cajon" };
+		palabra = diccionario[(int) (Math.random() * 6)]; // Esto es coger un palabra aleatoria
+
+		int fallos = 0;
+		System.out.println(palabra);
+		char[] guiones = mostrarguiones(palabra);
+		System.out.println(guiones);
+
+	}
+
+	public static char[] mostrarguiones(String palabra) {
+		int letrasPalabras = palabra.length(); // letras de la palabra oculta
+		char[] guiones = new char[letrasPalabras];
+
+		for (int i = 0; i < guiones.length; i++) {
+			guiones[i] = '_';
+		}
+		return guiones;
+	}
+}
