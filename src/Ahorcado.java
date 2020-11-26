17c93f16ece630fadabfbcde550142ee2ccc1977
Ivhod <56817761+Ivhod@users.noreply.github.com>
2020-11-26 11:33:48 +0100

    Add files via upload

--- src/Ahorcado.java
+++ src/Ahorcado.java
@@ -1,11 +1,12 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Ahorcado {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println(" Vamos a jugar al Ahorcado ");
-		System.out.println(" Se generara una Palabra aleatoria y tendras que aceptarlas");
+		System.out.println(" ||Vamos a jugar al Ahorcado|| ");
+		System.out.println(" Se generara una Palabra aleatoria y tendras que descubrirla.");
 
 		String palabra;
 		String[] diccionario = new String[] { "perro", "mando", "juntar", "portatil", "movil", "cajon" };
@@ -14,7 +15,41 @@ public class Ahorcado {
 		int fallos = 0;
 		System.out.println(palabra);
 		char[] guiones = mostrarguiones(palabra);
-		System.out.println(guiones);
+		boolean JuegoTerminado = false;
+		Scanner input = new Scanner(System.in);
+		do {
+
+			System.out.println(guiones);
+			System.out.println("Tienes " + fallos + " fallos ");
+			System.out.println("Introduce una letra para comprobar si esta en la palabra");
+			char letra = input.next().charAt(0);
+
+			boolean letraAcertada = false;
+			for (int i = 0; i < palabra.length(); i++) {
+				if (palabra.charAt(i) == letra) {
+					guiones[i] = letra;
+					letraAcertada = true;
+
+				}
+
+			}
+			if (!letraAcertada) {
+				System.out.println(" La letra " + letra + " no esta en la palabra");
+				fallos++;
+				if (fallos == 6) {
+					System.out.println("Has perdido porque has llegado al maximo de fallos y eres un inutil");
+					System.out.println("La palabra era " + palabra + " aprende a leer anda");
+					JuegoTerminado = true;
+				}
+			} else {
+				boolean ganar = !hayGuiones(guiones);
+				if (ganar) {
+					System.out.println("Has ganado la palabra era " + palabra);
+					JuegoTerminado = true;
+				}
+			}
+
+		} while (!JuegoTerminado);
 
 	}
 
@@ -22,9 +57,18 @@ public class Ahorcado {
 		int letrasPalabras = palabra.length(); // letras de la palabra oculta
 		char[] guiones = new char[letrasPalabras];
 
-		for (int i = 0; i < guiones.length; i++) { //Para que ponga guiones por el tamaño de la palabra
-			guiones[i] = '_';
+		for (int i = 0; i < guiones.length; i++) {
+			guiones[i] = '-';
 		}
 		return guiones;
+	}
+
+	public static boolean hayGuiones(char[] array) { // Comprobar si quedan guiones en la palabra si no hay terminamos
+														// el juego
+		for (char l : array) {
+			if (l == '-')
+				return true;
+		}
+		return false;
 	}
 }
