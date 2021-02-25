224373433a177e2c19d2c4b756a70d9c7e0d2beb
Ivhod <56817761+Ivhod@users.noreply.github.com>
2021-02-25 13:59:13 +0100

    Add files via upload

---
+++ Base de Datos/animelist.sql
@@ -0,0 +1,157 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.2
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 25-02-2021 a las 13:58:11
+-- Versión del servidor: 10.4.14-MariaDB
+-- Versión de PHP: 7.4.10
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `animelist`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `animes`
+--
+
+CREATE TABLE `animes` (
+  `id_anime` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `gender` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `animes`
+--
+
+INSERT INTO `animes` (`id_anime`, `name`, `gender`) VALUES
+(1, 'One Piece', 'Shonen'),
+(2, 'Naruto', 'Shonen'),
+(3, 'Dragon Ball Z', 'Shonen'),
+(4, 'Tokyo Ghoul', 'Shonen'),
+(5, 'Soul eater', 'Shonen'),
+(7, 'Fairy Tail', 'Shonen');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuarios`
+--
+
+CREATE TABLE `usuarios` (
+  `id_usuario` int(11) NOT NULL,
+  `usuario` varchar(30) NOT NULL,
+  `pass` varchar(100) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usuarios`
+--
+
+INSERT INTO `usuarios` (`id_usuario`, `usuario`, `pass`) VALUES
+(1, 'pepe', '1234'),
+(2, 'miguel', '1234'),
+(3, 'FRAN', '1234'),
+(4, 'CARMEN', '1234');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usu_ani`
+--
+
+CREATE TABLE `usu_ani` (
+  `usu_ani_id` int(11) NOT NULL,
+  `id_usuario` int(11) NOT NULL,
+  `id_anime` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Volcado de datos para la tabla `usu_ani`
+--
+
+INSERT INTO `usu_ani` (`usu_ani_id`, `id_usuario`, `id_anime`) VALUES
+(1, 1, 3),
+(2, 1, 2),
+(3, 1, 1),
+(4, 1, 4),
+(5, 2, 3),
+(6, 2, 4),
+(7, 3, 1),
+(8, 3, 3),
+(9, 3, 7);
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `animes`
+--
+ALTER TABLE `animes`
+  ADD PRIMARY KEY (`id_anime`);
+
+--
+-- Indices de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- Indices de la tabla `usu_ani`
+--
+ALTER TABLE `usu_ani`
+  ADD PRIMARY KEY (`usu_ani_id`),
+  ADD KEY `id_usuario` (`id_usuario`),
+  ADD KEY `id_anime` (`id_anime`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `animes`
+--
+ALTER TABLE `animes`
+  MODIFY `id_anime` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `usu_ani`
+--
+ALTER TABLE `usu_ani`
+  MODIFY `usu_ani_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `usu_ani`
+--
+ALTER TABLE `usu_ani`
+  ADD CONSTRAINT `usu_ani_ibfk_1` FOREIGN KEY (`id_usuario`) REFERENCES `usuarios` (`id_usuario`),
+  ADD CONSTRAINT `usu_ani_ibfk_2` FOREIGN KEY (`id_anime`) REFERENCES `animes` (`id_anime`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
