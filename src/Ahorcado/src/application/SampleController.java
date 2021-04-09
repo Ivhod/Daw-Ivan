bb50a2a2035ec0bc38b5391e5c7b6b808cc25f97
Ivhod <56817761+Ivhod@users.noreply.github.com>
2021-04-09 14:02:29 +0200

    Add files via upload

---
+++ src/Ahorcado/src/application/SampleController.java
@@ -0,0 +1,14 @@
+package application;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
+public class SampleController {
+	@FXML private Button Btq;
+	@FXML private Button Btw;
+	@FXML private Button Bte;
+	@FXML private Button Btr;
+	@FXML private Button Btt;
+	@FXML private Button Bty;
+	
+}
