972b857ce68ce0e89efe7b44f3d6b263032e1c4c
Ivhod <56817761+Ivhod@users.noreply.github.com>
2021-04-14 23:25:46 +0200

    Add files via upload

--- src/Ahorcado/src/application/SampleController.java
+++ src/Ahorcado/src/application/SampleController.java
@@ -1,38 +1,25 @@
 package application;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.image.Image;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 
 public class SampleController {
-	@FXML private Button Btq;
-	@FXML private Button Btw;
-	@FXML private Button Bte;
-	@FXML private Button Btr;
-	@FXML private Button Btt;
-	@FXML private Button Bty;
-	@FXML private Button Btu;
-	@FXML private Button Bti;
-	@FXML private Button Bto;
-	@FXML private Button Btp;
-	@FXML private Button Bta;
-	@FXML private Button Bts;
-	@FXML private Button Btd;
-	@FXML private Button Btf;
-	@FXML private Button Btg;
-	@FXML private Button Bth;
-	@FXML private Button Btj;
-	@FXML private Button Btk;
-	@FXML private Button Btl;
-	@FXML private Button Btñ;
-	@FXML private Button Bz;
-	@FXML private Button Btx;
-	@FXML private Button Btc;
-	@FXML private Button Btv;
-	@FXML private Button Btb;
-	@FXML private Button Btn;
-	@FXML private Button Btm;
+	
+	@FXML private Button btQ;
+	@FXML private Button btA;
+	@FXML private Button btZ;
+	
+	@FXML private Label titulo;
+	@FXML private TextField textFallos;
 	
 	
+	@FXML private ImageView image1;
 	
+	@FXML private void accionMostrar(ActionEvent evento) {
+		
+	}
 }
